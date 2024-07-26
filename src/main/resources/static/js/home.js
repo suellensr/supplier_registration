1cd10bb961caf3caee59e74492b031cd363558ef
suellensr <suellensreis@gmail.com>
2024-07-26 19:21:58 -0300

    implementing home

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    // Carregar fornecedores ao carregar a página
+    loadSuppliers();
+
+    // Botão de logout
+    $('#logout').on('click', function() {
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+    });
+
+    // Botão para cadastrar fornecedor
+    $('#supplierRegisterBtn').on('click', function() {
+        window.location.href = '/supplier-register';
+    });
+});
+
+function loadSuppliers() {
+    $.ajax({
+        url: '/get-suppliers', // Endpoint para obter os fornecedores
+        type: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        success: function(data) {
+            const supplierTableBody = $('#supplierTableBody');
+            supplierTableBody.empty(); // Limpar a tabela antes de adicionar os dados
+
+            data.forEach(supplier => {
+                const supplierRow = `
+                    <tr>
+                        <!-- ><td>${supplier.id}</td><-->
+                        <td>${supplier.supplierName}</td>
+                        <td>${supplier.contactName}</td>
+                        <td>${supplier.contactEmail}</td>
+                        <td>${supplier.documentNumber}</td>
+                        <td>
+                            <a class="btn btn-info btn-sm" href="/view-supplier/${supplier.id}"><i class="bi bi-eye"></i></a>
+                            <a class="btn btn-warning btn-sm" href="/supplier-edit/${supplier.id}"><i class="bi bi-pencil"></i></a>
+                            <!-- ><td><button class="btn btn-danger btn-sm" onclick="deleteSupplier(${supplier.id})"></button><-->
+                            <a href="#" id="delete" class="btn btn-danger btn-sm" ><i class="bi bi-trash"></i>
+                        </td>
+                    </tr>
+                `;
+                supplierTableBody.append(supplierRow);
+            });
+        },
+        error: function(error) {
+            console.error('Erro ao carregar fornecedores:', error);
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+    });
+
+    function deleteSupplier(id) {
+        if (confirm('Você tem certeza que quer deletar este fornecedor?')) {
+            $.ajax({
+                url: `/delete/${id}`,
+                type: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                success: function () {
+                    alert('Fornecedor deletado com sucesso!');
+                    loadSuppliers(); // Recarregar a lista de fornecedores
+                },
+                error: function (error) {
+                    console.error('Erro ao deletar fornecedor:', error);
+                }
+            });
+        }
+    }
+}
